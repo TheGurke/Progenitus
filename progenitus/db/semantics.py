2b9d8daee82409520d0d68b20c7389e8838409f2
TheGurke <thegurke@progenitus.org>
2011-09-30 13:51:25 +0200

    more token semantics; but needs more work

--- progenitus/db/semantics.py
+++ progenitus/db/semantics.py
@@ -8,7 +8,8 @@ _text_num = {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6,
 	"seven":7, "eight":8, "nine":9, "ten":10, "eleven":11, "twelve":12}
 
 _re_num = re.compile(r'[\d]+')
-_re_token = re.compile(r'[pP]ut an? (.*?) creature token onto the battlefield')
+_re_token = re.compile(r'[pP]ut [\S]+ (.*?) creature tokens? (?:with .*?|)'
+	'onto the battlefield')
 _counter1 = '%s enters the battlefield with ([\S]*?) (.*?) counters on it.'
 _counter2 = '[pP]ut an? (.*?) counter on (?:%s|this permanent)'
 
@@ -26,7 +27,7 @@ def init_carditem(item):
 	match = _re_token.findall(card.text)
 	if match is not None:
 		item.creates_tokens = match
-		print match
+#		print match
 	
 	# Has counters?
 	match = re.search(_counter1 % card.name, card.text)
