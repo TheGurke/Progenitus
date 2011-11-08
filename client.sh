df008acd9c98ed159e7fc442a0c6941b173962d8
TheGurke <thegurke@progenitus.org>
2011-11-08 23:48:52 +0100

    tell the startup scripts to pass arbitrary number of parameters

--- client.sh
+++ client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-./progenitus.py --client $1 $2 $3
+./progenitus.py $* --client
--- editor.sh
+++ editor.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-./progenitus.py --editor $1 $2 $3
+./progenitus.py $* --editor
