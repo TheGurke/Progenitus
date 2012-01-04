02a6ca9d88d1bfe0133550125a3236fbca90dbbf
TheGurke <thegurke@progenitus.org>
2012-01-04 18:40:57 +0100

    added sql script that fixes the missing planeswalker loyalties

---
+++ planeswalker fix.sql
@@ -0,0 +1,22 @@
+/*
+ * This adds loyalty points to the plainswalkers that are lacking them on
+ * magiccards.info.
+ */
+UPDATE "cards" SET "toughness" = 4 WHERE "id" = "zen.111";   /* Sorin Markov            */
+UPDATE "cards" SET "toughness" = 5 WHERE "id" = "zen.120";  /* Chandra Ablaze          */
+UPDATE "cards" SET "toughness" = 2 WHERE "id" = "zen.170";  /* Nissa Revane            */
+UPDATE "cards" SET "toughness" = 3 WHERE "id" = "wwk.31";   /* Jace, the Mind Sculptor */
+UPDATE "cards" SET "toughness" = 6 WHERE "id" = "roe.21";   /* Gideon Jura             */
+UPDATE "cards" SET "toughness" = 7 WHERE "id" = "roe.214";  /* Sarkhan the Mad         */
+UPDATE "cards" SET "toughness" = 4 WHERE "id" = "som.6";    /* Elspeth Tirel           */
+UPDATE "cards" SET "toughness" = 3 WHERE "id" = "som.94";   /* Koth of the Hammer      */
+UPDATE "cards" SET "toughness" = 3 WHERE "id" = "som.135";  /* Venser, the Sojourner   */
+UPDATE "cards" SET "toughness" = 6 WHERE "id" = "nph.1";    /* Karn Liberated          */
+UPDATE "cards" SET "toughness" = 6 WHERE "id" = "m12.16";   /* Gideon Jura             */
+UPDATE "cards" SET "toughness" = 4 WHERE "id" = "m12.58";   /* Jace, Memory Adept      */
+UPDATE "cards" SET "toughness" = 4 WHERE "id" = "m12.109";   /* Sorin Markov            */
+UPDATE "cards" SET "toughness" = 3 WHERE "id" = "m12.124";  /* Chandra, the Firebrand  */
+UPDATE "cards" SET "toughness" = 3 WHERE "id" = "m12.174";  /* Garruk, Primal Hunter   */
+UPDATE "cards" SET "toughness" = 3 WHERE "id" = "isd.105";  /* Liliana of the Veil     */
+UPDATE "cards" SET "toughness" = 3 WHERE "id" = "isd.181a"; /* Garruk Relentless       */
+
